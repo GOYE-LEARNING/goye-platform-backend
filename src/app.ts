a7f3e743df01012ea34f87e693bf928c35206bf7
seanimayi <seanimayi@gmail.com>
2025-11-08 16:45:14 +0100

    Done

--- src/app.ts
+++ src/app.ts
@@ -1,7 +1,7 @@
-import express, { type Request, type Response } from "express";
+import express, { Request, Response } from "express";
 import cors from "cors";
 import swaggerUi from "swagger-ui-express";
-import { RegisterRoutes } from "./routes/routes";
+import { RegisterRoutes } from "./routes/routes"; // No .js extension
 import bodyParser from "body-parser";
 
 const PORT = process.env.PORT || 4000;
@@ -11,8 +11,9 @@ app.use(express.json());
 app.use(cors());
 app.use(bodyParser.json());
 
-// Swagger documentation
-app.use('/docs', swaggerUi.serve, swaggerUi.setup(require('./routes/swagger.json')));
+// Use require for JSON files in CommonJS
+const swaggerDocument = require('./routes/swagger.json');
+app.use('/docs', swaggerUi.serve, swaggerUi.setup(swaggerDocument));
 
 app.get("/", (req: Request, res: Response) => {
   res.send("GOYE IS RUNNING");
