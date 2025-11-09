058fdc3ef17cb39d32b7b0a5167d6d73ef688391
seanimayi <seanimayi@gmail.com>
2025-11-09 20:44:47 +0100

    Done

--- src/utils/utils.ts
+++ src/utils/utils.ts
@@ -2,7 +2,7 @@ import { Resend } from "resend";
 const resend = new Resend(process.env.RESEND_KEY)
 export const SendEmail = async (to: string, subject: string, text: string) => {
     await resend.emails.send({
-        from: 'seanimayi@gmail.com',
+        from: 'GOYE Platform <onboarding@resend.dev>',
         to,
         subject,
         html: `<h1>${text}</h1>`
