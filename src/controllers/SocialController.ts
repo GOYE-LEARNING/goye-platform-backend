d03237fa34cb7f161223e6705bfa66441188f44d
seanimayi <seanimayi@gmail.com>
2025-11-20 14:28:39 +0100

    Reply done

--- src/controllers/SocialController.ts
+++ src/controllers/SocialController.ts
@@ -171,7 +171,7 @@ export class SocialController extends Controller {
   }
 
   @Security("bearerAuth")
-  @Post("/reply-other-reply{replyId}")
+  @Post("/reply-other-reply/{replyId}")
   public async ReplyOtherReply(
     @Body() body: Omit<ReplyDTO, "id">,
     @Path() replyId: string,
