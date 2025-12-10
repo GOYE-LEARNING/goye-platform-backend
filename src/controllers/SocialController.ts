d7c8fb014130a4fd9c7addad6e272ac480e6e1c0
seanimayi <seanimayi@gmail.com>
2025-12-10 14:12:14 +0100

    Joined

--- src/controllers/SocialController.ts
+++ src/controllers/SocialController.ts
@@ -1000,6 +1000,41 @@ export class SocialController extends Controller {
     }
   }
 
+  @Security('bearerAuth')
+  @Get("/check-joined/{groupId}")
+  public async CheckJoined(@Request() req: any, @Path() groupId: string) {
+    const userId = req.user?.id
+
+    if (!userId) {
+      return
+    }
+
+    if (!groupId) {
+      this.setStatus(400)
+      return {
+        message: "This group does not exist"
+      }
+    }
+
+    const isJoined = await prisma.joinedGroup.findUnique({
+      where: {
+        groupId_studentId: {
+          groupId,
+          studentId: userId
+        }
+      },
+      select: {
+        isJoined: true
+      }
+    })
+
+    this.setStatus(200)
+    return {
+      message: "Check group",
+      data: isJoined.isJoined
+    }
+  }
+
   @Security("bearerAuth")
   @Post("/join-group/{groupId}")
   public async JoinGroup(@Request() req: any, @Path() groupId: string) {
