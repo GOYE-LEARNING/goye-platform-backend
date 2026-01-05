1ff549d358234364a67bbf67d293c7f84488ab0f
seanimayi <seanimayi@gmail.com>
2026-01-05 19:41:00 +0100

    Done 2

--- prisma.config.ts
+++ prisma.config.ts
@@ -1,11 +1,12 @@
-import type { PrismaConfig } from "prisma";
-import { env } from "prisma/config";
-export default {
-  schema: "prisma/schema.prisma",
+import 'dotenv/config';
+import { defineConfig, env } from 'prisma/config';
+
+export default defineConfig({
+  schema: 'prisma/schema.prisma',
   migrations: {
-    path: "prisma/migrations",
-  },  
-  datasource: {
-    url: env("DATABASE_URL")
+    path: 'prisma/migrations',
   },
-} satisfies PrismaConfig;
+  datasource: {
+    url: env('DATABASE_URL'),
+  },
+});
