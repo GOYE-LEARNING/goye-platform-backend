0a4cd9a03d97abda577c053c598ecead0340bc5e
seanimayi <seanimayi@gmail.com>
2026-01-08 16:42:05 +0100

    Fixed and done 3

--- prisma.config.ts
+++ prisma.config.ts
@@ -1,12 +1,12 @@
-import "dotenv/config";
-import type { PrismaConfig } from "prisma";
-import { env } from "prisma/config";
+import type { PrismaConfig } from 'prisma';
+
 export default {
-  schema: "prisma/schema.prisma",
+  schema: 'prisma/schema.prisma',
   migrations: {
-    path: "prisma/migrations",
+    path: 'prisma/migrations',
   },
   datasource: {
-    url: env("DATABASE_URL")
+    // assumes Render injects DATABASE_URL as an environment variable
+    url: process.env.DATABASE_URL!, 
   },
 } satisfies PrismaConfig;
