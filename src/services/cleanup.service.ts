e5d18e2cd1d4353c87e1c87e807fb16c7b099525
seanimayi <seanimayi@gmail.com>
2026-03-09 20:33:35 +0100

    Updated once more

--- src/services/cleanup.service.ts
+++
@@ -1,9 +0,0 @@
-import { SessionService } from './session.service';
-
-export const startCleanupJob = () => {
-  // Run every hour
-  setInterval(async () => {
-    console.log('🧹 Running cleanup of expired sessions...');
-    await SessionService.cleanupExpiredSessions();
-  }, 60 * 60 * 1000); // 1 hour
-};
