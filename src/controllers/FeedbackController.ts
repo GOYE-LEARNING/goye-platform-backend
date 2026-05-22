0ee340d8afa1e0799438c2073ff4b1d99b2c2a7b
seanimayi <seanimayi@gmail.com>
2026-05-22 21:59:14 +0100

    Added Feedback API

---
+++ src/controllers/FeedbackController.ts
@@ -0,0 +1,78 @@
+import {
+  Body,
+  Controller,
+  Get,
+  Post,
+  Request,
+  Route,
+  Security,
+  Tags,
+} from "tsoa";
+import prisma from "../db";
+
+@Tags("Feed back Controller")
+@Route("feedback")
+export class FeedbackController extends Controller {
+  @Security("bearerAuth")
+  @Post("/feedback")
+  public async Feedback(
+    @Body() body: { message: string },
+    @Request() req: any,
+  ) {
+    const userId = req.user?.id;
+    const orgId = req.org?.id;
+    try {
+      if (!userId || !orgId) {
+        this.setStatus(401);
+        return {
+          message: "This party is unauthorized",
+        };
+      }
+      await prisma.feedback.create({
+        data: {
+          message: body.message,
+          ...(userId && { userId }),
+          ...(orgId && { organizationId: orgId }),
+        },
+      });
+
+      return {
+        message: "Feedback sent successfully",
+      };
+    } catch (error) {
+      this.setStatus(500);
+      console.error(error.message);
+    }
+  }
+
+  @Security("bearerAuth")
+  @Get("/fetch-feedbacks")
+  public async FetchFeedback(@Request() req: any) {
+    const userId = req.user?.id;
+    const orgId = req.org?.id;
+
+    try {
+      if (!userId || !orgId) {
+        this.setStatus(401);
+        return {
+          message: "This party is unauthorized",
+        };
+      }
+
+      const feedBacks = await prisma.feedback.findMany({
+        include: {
+          user: true,
+          organization: true,
+        },
+      });
+
+      return {
+        message: "Fetched successfully",
+        data: feedBacks,
+      };
+    } catch (error) {
+      this.setStatus(500);
+      console.error(error.message);
+    }
+  }
+}
